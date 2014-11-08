fdab7614127d11cc98d7680242adb98811fa56e3
milktrader <milktrader@gmail.com>
2014-11-08 16:09:52 -0500

    added additional pending tests for methods requiring two sets of returns

--- test/methods.jl
+++ test/methods.jl
@@ -29,8 +29,16 @@ end
 
 facts("ratio algorithms are correct") do
 
-  context("burke_ratio") do
-      @pending burke_ratio(Ra) =>  -0.4761482 # R's PerformanceAnalytics result
+  context("adjusted_sharpe_ratio") do
+      @pending adjusted_sharpe_ratio(Ra) => -0.4875318 # R's PerformanceAnalytics result
+  end
+
+  context("appraisal_ratio") do
+      @pending appraisal_ratio(Ra, Rb) => -0.6827613 # R's PerformanceAnalytics result
+  end
+
+  context("bernardo_ledoit_ratio") do
+      @pending bernardo_ledoit_ratio(Ra) => 0.9070269 # R's PerformanceAnalytics result
   end
 
   context("calmar_ratio") do
@@ -75,6 +83,10 @@ facts("ratio algorithms are correct") do
       @pending sortino_ratio(Ra) => -0.04009706 # R's PerformanceAnalytics result
   end
 
+  context("treynor_ratio") do
+      @pending treynor_ratio(Ra,Rb) => -4.671493 # R's PerformanceAnalytics result
+  end
+
   context("upside_potential_ratio") do
       @pending upside_potential_ratio(Ra) => 0.6002809 # R's PerformanceAnalytics result
   end
@@ -101,6 +113,10 @@ end
 
 facts("other algorithms are correct") do
 
+  context("active_premium") do
+      @pending active_premium(Ra,Rb) => -0.5434139 # R's PerformanceAnalytics result 
+  end
+
   context("annualized_return") do
       @fact annualized_return(Ra, method="arithmetic") =>  -0.4238347274496489 # R's PerformanceAnalytics result -0.4238922
       @fact annualized_return(Ra, method="geometric")  =>  -0.5612879122675767 # R's PerformanceAnalytics result -0.5612879
@@ -118,6 +134,30 @@ facts("other algorithms are correct") do
       @pending expected_shortfall(Ra) => -0.2831509 # R's PerformanceAnalytics result
   end
 
+  context("fama_beta") do
+      @pending fama_beta(Ra,Rb) => 1.924636 # R's PerformanceAnalytics result
+  end
+
+  context("lower_partial_moment") do
+      @pending lower_partial_moment(Ra,Rb) => 1.964494 # R's PerformanceAnalytics result
+  end
+
+  context("m2_sortina") do
+      @pending m2_sortina(Ra,Rb) => -0.5470395 # R's PerformanceAnalytics result
+  end
+
+  context("msquared") do
+      @pending msquared(Ra,Rb) => -0.2916333 # R's PerformanceAnalytics result
+  end
+
+  context("msquared_excess") do
+      @pending msquared_excess(Ra,Rb) => -0.2787415 # R's PerformanceAnalytics result
+  end
+
+  context("modigliani") do
+      @pending modigliani(Ra,Rb) => -0.0008739896 # R's PerformanceAnalytics result
+  end
+
   context("max_drawdown") do
       @pending max_drawdown(Ra) => 0.9029059 # R's PerformanceAnalytics result
   end
@@ -126,12 +166,28 @@ facts("other algorithms are correct") do
       @pending mean_absolute_deviation(Ra) => 0.0344848 # R's PerformanceAnalytics result
   end
 
+  context("net_selectivity") do
+      @pending net_selectivity(Ra,Rb) => -0.5268869 # R's PerformanceAnalytics result
+  end
+
   context("omega") do
       @pending omega(Ra) => 0.9070269 # R's PerformanceAnalytics result
   end
 
+  context("selectivity") do
+      @pending selectivity(Ra,Rb) => -0.5591403 # R's PerformanceAnalytics result
+  end
+
   context("semi_deviation") do
       @pending semi_deviation(Ra) => 0.04123659 # R's PerformanceAnalytics result
+  end
+
+  context("specific_risk") do
+      @pending specific_risk(Ra,Rb) => 0.8189397 # R's PerformanceAnalytics result
+  end
+
+  context("total_risk") do
+      @pending total_risk(Ra,Rb) => 0.8205434 # R's PerformanceAnalytics result
   end
 
   context("upside_frequency") do
