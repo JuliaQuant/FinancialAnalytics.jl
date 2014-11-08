58a58d860794e19362a06467f07df45a71ec3a46
milktrader <milktrader@gmail.com>
2014-11-08 15:45:03 -0500

    updated test results to match PerformanceAnaltyics

--- test/methods.jl
+++ test/methods.jl
@@ -30,11 +30,11 @@ end
 facts("ratio algorithms are correct") do
 
   context("burke_ratio") do
-      @pending burke_ratio(Ra) => -0.4743222 # R's PerformanceAnalytics result
+      @pending burke_ratio(Ra) =>  -0.4761482 # R's PerformanceAnalytics result
   end
 
   context("calmar_ratio") do
-      @pending calmar_ratio(Ra) => -0.6208448 # R's PerformanceAnalytics result
+      @pending calmar_ratio(Ra) => -0.6216461 # R's PerformanceAnalytics result
   end
 
   context("d_ratio") do
@@ -42,11 +42,11 @@ facts("ratio algorithms are correct") do
   end
 
   context("kelly_ratio") do
-      @pending kelly_ratio(Ra) => -0.3141632 # R's PerformanceAnalytics result
+      @pending kelly_ratio(Ra) => -0.3141626 # R's PerformanceAnalytics result
   end
 
   context("martin_ratio") do
-      @pending martin_ratio(Ra) => -0.4974161 # R's PerformanceAnalytics result
+      @pending martin_ratio(Ra) => -0.4975598 # R's PerformanceAnalytics result
   end
 
   context("omega_sharpe_ratio") do
@@ -54,48 +54,48 @@ facts("ratio algorithms are correct") do
   end
 
   context("pain_ratio") do
-      @pending pain_ratio(Ra) => -0.563304 # R's PerformanceAnalytics result
+      @pending pain_ratio(Ra) => -0.562903 # R's PerformanceAnalytics result
   end
 
   context("prospect_ratio") do
-      @pending prospect_ratio(Ra, mar=0) => -0.5786125 # R's PerformanceAnalytics result
+      @pending prospect_ratio(Ra, mar=0) => -0.579192 # R's PerformanceAnalytics result
   end
 
   context("sharpe_ratio") do
-      @pending sharpe_ratio(Ra, method=std) => -0.032477709 # R's PerformanceAnalytics result
-      @pending sharpe_ratio(Ra, method=var) => -0.018203728 # R's PerformanceAnalytics result
-      @pending sharpe_ratio(Ra, method=es) => -0.005944104  # R's PerformanceAnalytics result
+      @pending sharpe_ratio(Ra, method=std) => -0.032510205 # R's PerformanceAnalytics result
+      @pending sharpe_ratio(Ra, method=var) => -0.018213539 # R's PerformanceAnalytics result
+      @pending sharpe_ratio(Ra, method=es)  => -0.005940691 # R's PerformanceAnalytics result
   end
 
   context("skewness_kurtosis_ratio") do
-      @pending skewness_kurtosis_ratio(Ra) => -0.09644818 # R's PerformanceAnalytics result
+      @pending skewness_kurtosis_ratio(Ra) => -0.09654119 # R's PerformanceAnalytics result
   end
 
   context("sortino_ratio") do
-      @pending sortino_ratio(Ra) => -0.04005694 # R's PerformanceAnalytics result
+      @pending sortino_ratio(Ra) => -0.04009706 # R's PerformanceAnalytics result
   end
 
   context("upside_potential_ratio") do
-      @pending upside_potential_ratio(Ra) =>  0.6002809 # R's PerformanceAnalytics result
+      @pending upside_potential_ratio(Ra) => 0.6002809 # R's PerformanceAnalytics result
   end
 end
 
 facts("index algorithms are correct") do
 
   context("hurst_index") do
-      @pending hurst_indes(Ra) => 0.4087577 # R's PerformanceAnalytics result
+      @pending hurst_indes(Ra) => 0.4087282 # R's PerformanceAnalytics result
   end
 
   context("pain_index") do
-      @pending pain_index(Ra) => 0.9951365 # R's PerformanceAnalytics result
+      @pending pain_index(Ra) => 0.9971307 # R's PerformanceAnalytics result
   end
 
   context("smoothing_index") do
-      @pending smoothing_index(Ra) => 0.9825384 # R's PerformanceAnalytics result
+      @pending smoothing_index(Ra) => 0.9825174 # R's PerformanceAnalytics result
   end
 
   context("ulcer_index") do
-      @pending ulcer_index(Ra) => 1.126953 # R's PerformanceAnalytics result
+      @pending ulcer_index(Ra) => 1.128081 # R's PerformanceAnalytics result
   end
 end
 
@@ -107,15 +107,15 @@ facts("other algorithms are correct") do
   end
 
   context("downside_deviation") do
-      @pending downside_deviation(Ra) => 0.04190903 # R's PerformanceAnalytics result
+      @pending downside_deviation(Ra) => 0.041951 # R's PerformanceAnalytics result
   end
 
   context("downside_frequency") do
-      @pending downside_frequency(Ra) => 0.508 # R's PerformanceAnalytics result
+      @pending downside_frequency(Ra) => 0.509018 # R's PerformanceAnalytics result
   end
 
   context("expected_shortfall") do
-      @pending expected_shortfall(Ra) => -0.2824223 # R's PerformanceAnalytics result
+      @pending expected_shortfall(Ra) => -0.2831509 # R's PerformanceAnalytics result
   end
 
   context("max_drawdown") do
@@ -123,7 +123,7 @@ facts("other algorithms are correct") do
   end
 
   context("mean_absolute_deviation") do
-      @pending mean_absolute_deviation(Ra) => 0.03441915 # R's PerformanceAnalytics result
+      @pending mean_absolute_deviation(Ra) => 0.0344848 # R's PerformanceAnalytics result
   end
 
   context("omega") do
@@ -131,19 +131,19 @@ facts("other algorithms are correct") do
   end
 
   context("semi_deviation") do
-      @pending semi_deviation(Ra) => 0.04119674 # R's PerformanceAnalytics result
+      @pending semi_deviation(Ra) => 0.04123659 # R's PerformanceAnalytics result
   end
 
   context("upside_frequency") do
-      @pending upside_frequency(Ra) => 0.464 # R's PerformanceAnalytics result
+      @pending upside_frequency(Ra) => 0.4649299 # R's PerformanceAnalytics result
   end
 
   context("upside_risk") do
-      @pending upside_risk(Ra) => 0.03021397 # R's PerformanceAnalytics result
+      @pending upside_risk(Ra) => 0.03024423 # R's PerformanceAnalytics result
   end
 
   context("value_at_risk") do
-      @pending value_at_risk(Ra) => -0.09221999 # R's PerformanceAnalytics result
+      @pending value_at_risk(Ra) => -0.09235503 # R's PerformanceAnalytics result
   end
 
   context("volatility_skewness") do
